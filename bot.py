e19db4f467aa01f9a3e2e738445849b54de7b7fd
kaifsarkar123 <139149350+kaifsarkar123@users.noreply.github.com>
2023-11-07 12:34:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -377,6 +377,7 @@ def run_bot():
         .build()
     )
 
+    user_filter = filters.Chat(chat_id=user_ids)
     msg_filter = filters.TEXT
 
     handler_list = [
@@ -389,7 +390,7 @@ def run_bot():
         CommandHandler("model", change_model, user_filter),
         CommandHandler("temp", change_temperature, user_filter),
         CommandHandler("cutoff", change_cutoff, user_filter),
-        MessageHandler(user_filter & msg_filter, recv_msg),
+        MessageHandler(msg_filter, recv_msg),
         CallbackQueryHandler(view_other_drafts),
     ]
     for handler in handler_list:
