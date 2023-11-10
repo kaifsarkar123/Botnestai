cb64714fb8772c5d8d29c49f188b826453cf4ba4
kaifsarkar123 <139149350+kaifsarkar123@users.noreply.github.com>
2023-11-10 22:25:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -159,7 +159,7 @@ async def recv_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if input_text == "":
         return await update.message.reply_text("❌ Empty message.")
-    m=await message.reply_sticker("CAACAgIAAxkBAAEnjMFlTl6pcMJDvFHvD2YeoTkQRxs_NQACIAAD9wLID1KiROfjtgxPMwQ")
+    message = await update.message.reply_text("📝")
     context.chat_data[mode]["last_input"] = input_text
     context.chat_data[mode]["last_msg_id"] = message.message_id
 
