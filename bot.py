9e5026e32c09fae27cc62ddb819c40e080eb8448
kaifsarkar123 <139149350+kaifsarkar123@users.noreply.github.com>
2023-11-18 12:05:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -180,7 +180,9 @@ async def recv_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if input_text == "":
         return await update.message.reply_text("❌ Empty message.")
     message = await update.message.reply_text("...")
-    await message.edit_text("Please wait")
+    await message.edit_text("Please wait.")
+    await message.edit_text("Please wait..")
+    await message.edit_text("Please wait...")
     context.chat_data[mode]["last_input"] = input_text
     context.chat_data[mode]["last_msg_id"] = message.message_id
 
